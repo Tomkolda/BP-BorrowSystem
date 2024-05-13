4752f34e5bad97c14a81a0c6f813778e419eeff7
Tomáš Koláček <80208930+Tomkolda@users.noreply.github.com>
2024-05-13 14:53:05 +0200

    Add files via upload

---
+++ Pujcocna/CultureControler.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Localization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Půjčovna
+
+
+{
+    [Microsoft.AspNetCore.Mvc.Route("/[controller]")]
+    [ApiController]
+    public class CultureControler : ControllerBase
+    {
+        public ActionResult SetCulture()
+        {
+            IRequestCultureFeature culture = HttpContext.Features.Get<IRequestCultureFeature>();
+            string refererUrl = Request.Headers["Referer"];
+            Response.Cookies.Append(
+                CookieRequestCultureProvider.DefaultCookieName,
+                CookieRequestCultureProvider.MakeCookieValue(
+                    new RequestCulture(new string[] { "en-US", "cs-CZ" }
+                    .Where(option => option != culture.RequestCulture.Culture.Name)
+                    .FirstOrDefault())),
+                new CookieOptions { Expires = DateTimeOffset.UtcNow.AddYears(1) });
+
+
+            return Redirect(refererUrl);
+        }
+    }
+}
